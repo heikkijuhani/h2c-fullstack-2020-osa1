37ac3885b256ce4f202bdda93a4849e3350c3c0a
heikkijuhani <57320479+heikkijuhani@users.noreply.github.com>
2021-01-14 19:01:44 +0200

    Final commit

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -1,17 +1,57 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const name = 'Pekka'
-  const age = 10
-
+const Header = (props) => {
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Footer />
-    </>
+    <div>
+      {props.course}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      {props.part} {props.exercises}
+    </div>
+  )
+}
+
+const Content = (props) => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
+    </div>
+  )
+}
+
+const Total = () => {
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  return (
+    <div>
+      Number of exercises {exercises1 + exercises2 + exercises3}
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  return (
+    <div>
+      <Header course={course}/>
+      <Content />
+      <Total />
+    </div>
   )
 }
 
